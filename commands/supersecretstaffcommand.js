494c44bc4a8b26b210546debb28df785fb76176e
ReubenDollmanYT <reubendollman142432@gmail.com>
2022-07-27 12:54:46 +1200

    e

---
+++ commands/supersecretstaffcommand.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'supersecretstaffcommand',
+	description: ':eyes:',
+	async execute(message) {
+    	message.channel.send("https://tenor.com/view/hugs-rickroll-gif-24588121")
+ }
+};	
